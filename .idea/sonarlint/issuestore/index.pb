45fb0c94c717535bcb3d917f3925f316900c52e7
Alex Gochenour <alexgochenour@gmail.com>
2022-09-09 17:28:26 -0400

    SETUP

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+K
+smalletsPowerAboveNumber.go,a/5/a5a4be68493c9a10e175e554909435cbcd1571d1
---
+++ reversedWords.go
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/51c8991dee245d7ddf00000e/train/go
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world!")
+}
