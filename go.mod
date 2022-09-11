f011cd39cae29fad568a57b6d5bd1a1498faa2c7
Alex Gochenour <alexgochenour@gmail.com>
2022-09-10 23:53:20 -0400

    success! first passing kata.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/chunyukuo88/kata_with_go
+
+go 1.19
--- reversedWords.go
+++ reversedWords.go
@@ -2,8 +2,19 @@
 
 package main
 
-import "fmt"
+import (
+	"strings"
+)
 
-func main() {
-	fmt.Println("Hello world!")
+func reverse(words []string) []string {
+	for i, j := 0, len(words)-1; i < j; i, j = i+1, j-1 {
+		words[i], words[j] = words[j], words[i]
+	}
+	return words
+}
+
+func ReverseWords(str string) string {
+	split := strings.Split(str, " ")
+
+	return strings.Join(reverse(split), " ")
 }
