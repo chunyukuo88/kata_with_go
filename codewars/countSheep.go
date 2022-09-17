14b39a102c93ed9da6a86f598ea97cc8e3b1307d
Alex Gochenour <alexgochenour@gmail.com>
2022-09-16 21:25:01 -0400

    completed countSheep kata.

--- codewars/countSheep.go
+++ codewars/countSheep.go
@@ -1,7 +1,25 @@
-// https://www.codewars.com/kata/5b077ebdaf15be5c7f000077/train/go
+/*
+https://www.codewars.com/kata/5b077ebdaf15be5c7f000077/train/go
+*/
 
 package codewars
 
-func countSheep(num int) string {
+import (
+	"strconv"
+	"strings"
+)
 
+func countSheep(num int) string {
+	if num == 0 {
+		return ""
+	}
+
+	var toBeJoined []string
+
+	for i := 1; i <= num; i++ {
+		numberPlusSheep := strconv.Itoa(i) + " sheep..."
+		toBeJoined = append(toBeJoined, numberPlusSheep)
+	}
+
+	return strings.Join(toBeJoined, "")
 }
