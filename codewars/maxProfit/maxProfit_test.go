bef5ff8970c2cc486ce35e9c2bccb4d6fead2e00
Alex Gochenour <alexgochenour@gmail.com>
2022-09-18 21:46:32 -0400

    feat: MinMax kata complete.

--- codewars/maxProfit/maxProfit_test.go
+++ codewars/maxProfit/maxProfit_test.go
@@ -4,9 +4,18 @@ import (
 	"testing"
 )
 
-func TestMinMax(t *testing.T) {
+func TestMinMax_consecutiveIntegers(t *testing.T) {
 	arg := []int{1, 2, 3, 4, 5}
 	want := [2]int{1, 5}
+	got := MinMax(arg)
+	if got != want {
+		t.Errorf("MinMax(%v) = %v; want %v", arg, got, want)
+	}
+}
+
+func TestMinMax_largeAndSmall(t *testing.T) {
+	arg := []int{2334454, 5}
+	want := [2]int{5, 2334454}
 	got := MinMax(arg)
 	if got != want {
 		t.Errorf("MinMax(%v) = %v; want %v", arg, got, want)
