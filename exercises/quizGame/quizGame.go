8f8bf883eb8c63cb26aa66074240090ad64649fc
Alex Gochenour <alexgochenour@gmail.com>
2022-09-27 22:30:12 -0400

    progress on reading the CSV file.

--- exercises/quizGame/quizGame.go
+++ exercises/quizGame/quizGame.go
@@ -1,12 +1,24 @@
-package quizgame
+package main
 
 import (
+	"encoding/csv"
+	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Quiz Game! Get ready.")
-	questionIndex := 0
-	answeredCorrectly := 0
+	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	flag.Parse()
 
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		fmt.Printf("Failed to open CSV file: %v", *csvFilename)
+		os.Exit(1)
+	}
+
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+
+	fmt.Printf("Here are the lines:\n%v", lines)
 }
