e707ae965d56672d1882c9deb4a1d7b1a85e35d9
Alex Gochenour <alexgochenour@gmail.com>
2022-10-01 21:18:29 -0400

    uninstalled ginkgo and gomega.

--- codewars/splitStrings/splitStrings_test.go
+++ codewars/splitStrings/splitStrings_test.go
@@ -1,10 +1,2 @@
 // https://www.codewars.com/kata/515de9ae9dcfc28eb6000001/train/go
 package splitstrings
-
-var _ = Describe("Split Strings", func() {
-	It("Basic tests", func() {
-		Expect(Solution("abc")).To(Equal([]string{"ab", "c_"}))
-		Expect(Solution("dawsd")).To(Equal([]string{"da", "ws", "d_"}))
-		Expect(Solution("awsaws")).To(Equal([]string{"aw", "sa", "ws"}))
-	})
-})
