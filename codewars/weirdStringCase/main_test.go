e6daafb86f5876f301d64c025d7f1e5c1ecff11e
chunyukuo88 <alexgochenour@gmail.com>
2022-10-02 22:09:52 -0400

    next kata

---
+++ codewars/weirdStringCase/main_test.go
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/52b757663a95b11b3d00062d/train/go
+package weirdStringCase
+
+import (
+	"testing"
+)
+
+func ToWeirdCase_singleWord(t *testing.T){
+	arg := "abc"
+	want := "AbC"
+	got := ToWeirdCase(arg)
+	if want != got {
+		t.Errorf("Wanted %v, got %v", want, got)
+	}
+}
+// func ToWeirdCase_singleWord(t *testing.T){
+
+// }
